5bc07143f059f6c16b93175688489c6d36a74c17
jelena-pavkovic <jelena.pavkovic28@gmail.com>
2026-03-17 11:56:59 +0100

    Register form

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -2,23 +2,26 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user.model';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // Za ngModel
+import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-register',standalone: true,
+  selector: 'app-register',
+  standalone: true,
   imports: [
     CommonModule,
     FormsModule,
+    RouterLink,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    RouterLink
+    MatSelectModule
   ],
   templateUrl: './register.html',
   styleUrl: './register.css',
@@ -35,10 +38,17 @@ export class Register {
     password: ''
   };
 
+  availableTypes: string[] = ['Slagalica', 'Slikovnica', 'Figura', 'Automobil', 'Edukativna igra'];
+
   constructor(private authService: AuthService, private router: Router) {}
 
   onRegister() {
-    this.authService.register(this.userData);
-    this.router.navigate(['/']);
+  if (this.userData.email && this.userData.password && this.userData.username) {
+      this.authService.register(this.userData);
+      alert('Uspešna registracija!');
+      this.router.navigate(['/']);
+    } else {
+      alert('Molimo popunite sva obavezna polja.');
+    }
   }
 }
